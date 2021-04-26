bff13134573500d8b08b6ed01d936c8e0f3f56ff
Jordi Gelderblom <0998323@hr.nl>
2021-04-26 13:05:28 +0200

    Createpoll page

---
+++ Controllers/PollController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_D.Controllers
+{
+    public class PollController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("create_poll")]
+        public IActionResult CreatePoll()
+        {
+            return View();
+        }
+    }
+}
