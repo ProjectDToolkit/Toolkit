43c7016ec21468daceee296bfba7c46043c83902
Jordi Gelderblom <0998323@hr.nl>
2021-05-09 22:18:49 +0200

    trying to get this stupid shit working

--- Controllers/PollController.cs
+++ Controllers/PollController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Project_D.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +10,40 @@ namespace Project_D.Controllers
 {
     public class PollController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+
+        MySqlConnection connection = new MySqlConnection(default);
         
         [Route("create_poll")]
         public IActionResult CreatePoll()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitPoll(PollModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    connection.Open();
+
+                    string q = $"INSERT INTO `polls`(`question`) VALUES ({model.Question})";
+
+                    MySqlCommand comm = new MySqlCommand(q, connection);
+                    MySqlDataReader reader = comm.ExecuteReader();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return RedirectToAction("CreatePoll");
         }
     }
 }
---
+++ Models/PollModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_D.Models
+{
+    public class PollModel
+    {
+        public string Question { get; set; }
+
+    }
+}
