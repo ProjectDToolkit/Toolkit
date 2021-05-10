48f893b4ec298891515aeb620d89255a11ab703d
timfeskens <0967290@hr.nl>
2021-05-10 10:38:13 +0200

    session code can be generated, session code can be pushed to new page

---
+++ Controllers/SessionStartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ProjectD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectD.Controllers
+{
+    public class SessionStartController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult Index(string SessionCode)
+		{
+			if (SessionCode == null)
+			{
+				ViewBag.Name = string.Format("No session code was posted.");
+			}
+			else
+			{
+				ViewBag.Name = string.Format("Posted Session code: {0}", SessionCode);
+			}
+			return View();
+		}
+	}
+}
