c20e4ad4767acf98afa9257a94df2ec1fd1f9161
trevorfly <trevorfly123@gmail.com>
2021-05-14 21:03:27 +0200

    Added MVC for uploading documents. Currently local Edited gitignore so that it would not push the folder for picutres

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ProjectD.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private readonly IWebHostEnvironment environment;
+        public IActionResult Index()
+        {
+        return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    string test = AppContext.BaseDirectory;
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(),"UploadedFiles", formFile.FileName);
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            return Ok(new { count = files.Count, size, filePaths });
+        }
+    }
+
+}
---
+++ Models/Upload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectD.Models
+{
+    public class Upload
+{
+}
+}
