b02b7cea104c6d19778ea0ba6304f898158a9ff5
trevorfly <trevorfly123@gmail.com>
2021-06-04 16:48:01 +0200

    Worked on making it localy to DB

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -6,6 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using ProjectD.Models;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Hosting.Internal;
+using ProjectD.Database;
+
 
 namespace ProjectD.Controllers
 {
@@ -14,9 +21,76 @@ namespace ProjectD.Controllers
         private readonly IWebHostEnvironment environment;
         public IActionResult Index()
         {
-        return View();
+            return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(FileUpload obj)
+        {
+            try
+            {
+                string strDateTime = System.DateTime.Now.ToString("ddMMyyyyHHMMss");
+                string finalPath = "\\wwwroot\\Files\\" + strDateTime + obj.UploadFile.fileName;
+
+                obj.UploadFile.SaveAs(Directory.GetCurrentDirectory() + ("~") + finalPath);
+                obj.FilePath = finalPath;
+                ViewBag.Message = SaveToDB(obj);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message.ToString();
+                return View();
+            }
+        }
+
+        public string SaveToDB(FileUpload obj)
+        {
+            try
+            {
+                HttpContext.Session.SetString("idSession", obj.IdSession);
+                HttpContext.Session.SetString("filename", obj.FileName);
+                HttpContext.Session.SetString("fileDesc", obj.FileDesc);
+                HttpContext.Session.SetString("filePath", obj.FilePath);
+                if (HttpContext.Session.GetString("SessionCode") != null)
+                {
+                    string idSession = HttpContext.Session.GetString("idSession");
+                    string fileName = HttpContext.Session.GetString("fileName");
+                    string fileDesc = HttpContext.Session.GetString("fileDesc");
+                    string filePath = HttpContext.Session.GetString("filePath");
+
+                    MySqlConnection Connection;
+                    Connection = new MySqlConnection(Connector.getString());
+                    Connection.Open();
+                
+                        string stringToInsert = @"INSERT INTO file (idSession, filName, filePath,   onDate) VALUES (@idSession, @filName, @filePath, GETDATE())";
+
+                    using (MySqlCommand command = new MySqlCommand(stringToInsert, Connection))
+                    {
+                        command.Parameters.AddWithValue("@idSession", idSession);
+                        command.Parameters.AddWithValue("@filName", fileName);
+                        command.Parameters.AddWithValue("@filePath", filePath);
+
+                        command.Prepare();
+                    }
+                    return "Saved Successfully";
+                }
+                else
+                {
+                    return string.Format("Error Something went wrong");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+    }
+}
+
+
+
+        /*
         [HttpPost("FileUpload")]
         public async Task<IActionResult> Index(List<IFormFile> files)
         {
@@ -36,8 +110,7 @@ namespace ProjectD.Controllers
                     }
                 }
             }
-            return Ok(new { count = files.Count, size, filePaths });
+            return View();
         }
-    }
+        */
 
-}
---
+++ Models/FileUpload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+
+namespace ProjectD.Models
+{
+    public class FileUpload
+    {
+        public string IdSession { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        [DisplayName("File Desc")]
+        public string FileDesc { get; set; }
+
+        [DisplayName("File Path")]
+        public string FilePath { get; set; }
+
+        public HttpPostedFileBase UploadFile { get; set; }
+    }
+}
